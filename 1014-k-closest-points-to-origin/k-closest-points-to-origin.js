a98589bddc3fba2fcab3ca66b8b45ecb52e688bf
Jignesh <jigneshgurav11@gmail.com>
2025-08-27 12:50:32 +0530

    Time: 28 ms (92.37%) | Memory: 72.5 MB (47.29%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kClosest = function(points, k) {
+  let distances = []
+
+    for(let i = 0; i < points.length ; i++){
+          let point = points[i]
+          let distance = (point[0]*point[0])+(point[1]*point[1])
+          distances.push([distance,point])
+    }
+   
+    distances.sort((a, b) => a[0] - b[0]);
+
+    
+    let result = [];
+    for (let i = 0; i < k; i++) {
+        result.push(distances[i][1]);
+    }
+
+    return result;
+    
+};
