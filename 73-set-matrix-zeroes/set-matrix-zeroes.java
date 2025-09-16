84006a7e66f109caaf6a6a3c9c33250e2af59297
Jignesh <jigneshgurav11@gmail.com>
2025-09-16 23:49:32 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (94.70%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,72 @@
+class Solution {
+
+    // public void setZeroes(int[][] matrix) {
+    //     int m = matrix.length;
+    //     int n = matrix[0].length;
+    //     boolean hasZero = false;
+
+    //     for (int i = 0; i < m; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             if (matrix[i][j] == 0) {
+    //                 hasZero = true;
+    //                 markRow(i, n, matrix);
+    //                 markCol(j, m, matrix);
+    //             }
+
+    //         }
+    //     }
+
+    //     if (hasZero) {
+    //         for (int i = 0; i < m; i++) {
+    //             for (int j = 0; j < n; j++) {
+    //                 if (matrix[i][j] == -1) {
+    //                     matrix[i][j] = 0;
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    // }
+
+    // public void markRow(int row, int n, int[][] matrix) {
+    //     for (int j = 0; j < n; j++) {
+    //         if (matrix[row][j] != 0) {
+    //             matrix[row][j] = -1;
+    //         }
+    //     }
+    // }
+
+    // public void markCol(int col, int m, int[][] matrix) {
+    //     for (int i = 0; i < m; i++) {
+    //         if (matrix[i][col] != 0) {
+    //             matrix[i][col] = -1;
+    //         }
+    //     }
+    // }
+
+    public void setZeroes(int [][] matrix){
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[] columns = new int[m];
+        int[] rows = new int[n];
+
+        for(int i = 0; i < m;i++){
+            for(int j = 0; j < n;j++){
+                if(matrix[i][j] == 0){
+                    columns[i] = 1;
+                    rows[j] = 1;
+                }
+            }
+        }
+          for(int i = 0; i < m;i++){
+            for(int j = 0; j < n;j++){
+                if(columns[i] == 1 || rows[j] == 1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+    }
+    
+}
