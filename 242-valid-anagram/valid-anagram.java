545d1cf9e8b2a215cde5b27684ffc93ba57b5330
Jignesh <jigneshgurav11@gmail.com>
2025-10-19 11:50:36 +0530

    Time: 27 ms (5.39%) | Memory: 43.1 MB (83.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,11 +4,9 @@ class Solution {
 
         Map<Character,Integer> map = new HashMap<>();
 
-        for(char c: s.toCharArray()){
-            map.put(c,map.getOrDefault(c,0)+1);
-        }
-        for(char c: t.toCharArray()){
-            map.put(c,map.getOrDefault(c,0)-1);
+        for(int i = 0 ; i < s.length();i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+            map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)-1);
         }
 
         for(Map.Entry<Character,Integer> entry:map.entrySet()){
