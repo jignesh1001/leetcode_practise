8edb9df54f7bdbb94c4475ebfdc8a5cab00beb9d
Jignesh <jigneshgurav11@gmail.com>
2025-10-19 23:24:58 +0530

    Time: 1042 ms (5.06%) | Memory: 47.6 MB (92.15%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -3,28 +3,27 @@ class Solution {
         List<List<String>> result = new ArrayList<>();
         boolean[] visited = new boolean[strs.length];
 
-        for (int i = 0; i < strs.length; i++) {
-            if (visited[i])
-                continue;
+        for(int i = 0; i < strs.length;i++){
+            if(visited[i]) continue;
 
             List<String> inner = new ArrayList<>();
             inner.add(strs[i]);
             visited[i] = true;
-            for (int j = i + 1; j < strs.length; j++) {
-                if (!visited[j] && isAnagram(strs[i], strs[j])) {
+
+            for(int j = i+1; j < strs.length;j++){
+                if(!visited[j] && isAnagram(strs[i],strs[j])){
                     inner.add(strs[j]);
                     visited[j] = true;
                 }
             }
             result.add(inner);
-
-        }
+        } 
+       
         return result;
     }
 
-    public static boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())
-            return false;
+    public static boolean isAnagram(String t, String s){
+        if(s.length() != t.length()) return false;
 
         char[] sArr = s.toCharArray();
         char[] tArr = t.toCharArray();
@@ -32,7 +31,6 @@ class Solution {
         Arrays.sort(sArr);
         Arrays.sort(tArr);
 
-        return Arrays.equals(sArr, tArr);
-
+        return Arrays.equals(sArr,tArr);
     }
 }
