19a480442970cb1530ba93aee6157fbe222b40dc
Jignesh <jigneshgurav11@gmail.com>
2025-10-19 22:55:03 +0530

    Time: 1102 ms (5.06%) | Memory: 47.6 MB (92.15%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -23,17 +23,16 @@ class Solution {
     }
 
     public static boolean isAnagram(String s, String t) {
-         if(s.length() != t.length()) return false;
-
+        if (s.length() != t.length())
+            return false;
 
         char[] sArr = s.toCharArray();
         char[] tArr = t.toCharArray();
 
-
         Arrays.sort(sArr);
         Arrays.sort(tArr);
 
-        return Arrays.equals(sArr,tArr);
+        return Arrays.equals(sArr, tArr);
 
     }
 }
