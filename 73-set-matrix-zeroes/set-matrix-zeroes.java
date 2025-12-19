6da291c95cefb2381151f56a8f9c74ed0268d043
Jignesh <jigneshgurav11@gmail.com>
2025-12-19 10:34:44 +0530

    Time: 1 ms (61.75%) | Memory: 47.5 MB (17.78%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,6 +2,7 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
+
         int[] row = new int[n];
         int[] col = new int[m];
 
@@ -13,12 +14,14 @@ class Solution {
                 }
             }
         }
-        for(int i = 0; i < n;i++){
+         for(int i = 0; i < n;i++){
             for(int j = 0; j < m;j++){
                 if(row[i] == 1 || col[j] == 1){
-                   matrix[i][j] = 0;
+                    matrix[i][j] = 0;
                 }
             }
         }
+
+        
     }
 }
