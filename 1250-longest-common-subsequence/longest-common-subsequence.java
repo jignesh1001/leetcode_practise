4801e0b48994f89b29eb5636b30bc1229535840b
Jignesh <jignesh09.dev@gmail.com>
2026-03-18 12:48:11 +0530

    Time: 20 ms (78.65%) | Memory: 53.8 MB (84.15%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,34 +1,28 @@
 class Solution {
-
-
     public int longestCommonSubsequence(String text1, String text2) {
-         int n = text1.length();
-        int m = text2.length();
+        int m = text1.length();
+        int n = text2.length();
 
-        int[][] t = new int[n + 1][m + 1];
+        int[][] t = new int[m+1][n+1];
 
-        for(int i = 0; i <= n; i++){
-            for(int j = 0; j <= m; j++){
-                t[i][j] = -1;
+        for(int i = 0; i < m+1;i++){
+            for(int j = 0; j < n+1;j++){
+                if(i == 0 || j == 0){
+                    t[i][j] = 0;
+                }
             }
         }
 
-        return lcs(text1, text2, n, m, t);
+        for(int i = 1; i < m+1;i++){
+            for(int j = 1; j < n+1;j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    t[i][j] = 1 + t[i-1][j-1];
+                }
+                else{
+                    t[i][j] = Math.max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        return t[m][n];
     }
-
-    public int lcs(String text1, String text2,int n,int m,int [][] t){
-        if(n == 0 || m == 0){
-            return 0;
-        }
-        if(t[n][m] != -1){
-            return t[n][m];
-        }
-        if(text1.charAt(n-1) == text2.charAt(m-1)){
-            return t[n][m] = 1+lcs(text1,text2,n-1,m-1,t);
-        }
-        else{
-            return t[n][m] = Math.max(lcs(text1,text2,n,m-1,t),lcs(text1,text2,n-1,m,t));
-        }
-    }
-
 }
