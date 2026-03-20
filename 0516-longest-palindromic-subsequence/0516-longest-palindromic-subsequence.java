93bf26c3ca69666771c0fcbe7e14c87290eabd44
Jignesh <jignesh09.dev@gmail.com>
2026-03-20 12:30:43 +0530

    Time: 29 ms (82.2%), Space: 63.5 MB (79.47%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -10,13 +10,13 @@ class Solution {
 
         int[][] t = new int[m+1][m+1];
         
-        for(int i =0; i<m+1;i++){
-            for(int j=0;j <m+1;j++){
-                if(i == 0 || j == 0){
-                    t[i][j] = 0;
-                }
-            }
-        }
+        // for(int i =0; i<m+1;i++){
+        //     for(int j=0;j <m+1;j++){
+        //         if(i == 0 || j == 0){
+        //             t[i][j] = 0;
+        //         }
+        //     }
+        // }
       
 
         for(int i =1; i <m+1;i++){
