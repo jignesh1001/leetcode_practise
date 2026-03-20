9f036bde7e2cbe038134acc632cb7970f1c6a49e
Jignesh <jignesh09.dev@gmail.com>
2026-03-20 11:18:15 +0530

    Time: 7 ms (71.33%) | Memory: 46.4 MB (61.88%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -5,13 +5,13 @@ class Solution {
 
         int[][] t = new int[m+1][n+1];
 
-        for(int i = 0; i < m+1;i++){
-            for(int j = 0; j < n+1;j++){
-                if(i == 0 || j == 0){
-                    t[i][j] = 0;
-                }
-            }
-        }
+        // for(int i = 0; i < m+1;i++){
+        //     for(int j = 0; j < n+1;j++){
+        //         if(i == 0 || j == 0){
+        //             t[i][j] = 0;
+        //         }
+        //     }
+        // }
 
         for(int i =1; i < m+1; i++){
             for(int j =1; j < n+1;j++){
