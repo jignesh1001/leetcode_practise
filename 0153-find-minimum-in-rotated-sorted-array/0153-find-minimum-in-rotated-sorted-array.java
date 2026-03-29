126699a5c796cacb01ae4a693b754e86b731b991
Jignesh <jignesh09.dev@gmail.com>
2026-03-29 19:34:23 +0530

    Time: 0 ms (100%), Space: 43.8 MB (48.18%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -10,7 +10,7 @@ class Solution {
                 start = mid + 1;
             }
             else{
-                end = mid -1;
+                end = mid ;
             }
         }
         return 0;
