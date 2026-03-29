7d6641e57a7136bd80233f9c8f349da9e53b982e
Jignesh <jignesh09.dev@gmail.com>
2026-03-29 19:43:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (48.18%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -29,7 +29,7 @@ class Solution {
             } else if (nums[mid] >= nums[start]) {
                 start = mid + 1;
             } else if (nums[mid] <= nums[end]) {
-                end = mid - 1;
+                end = mid -1;
             }
         }
         return 0;
