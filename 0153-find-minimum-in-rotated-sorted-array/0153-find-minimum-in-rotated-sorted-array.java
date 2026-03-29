b78e02b7d8a852d4b85e490fc1ffb2dcda655b31
Jignesh <jignesh09.dev@gmail.com>
2026-03-29 19:33:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -10,7 +10,7 @@ class Solution {
                 start = mid + 1;
             }
             else{
-                end = mid;
+                end = mid -1;
             }
         }
         return 0;
