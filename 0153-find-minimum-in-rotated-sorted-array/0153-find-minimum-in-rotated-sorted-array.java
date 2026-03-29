6558226bd809540ff363dde5f8d113d76f88cf60
Jignesh <jignesh09.dev@gmail.com>
2026-03-29 19:41:22 +0530

    Time: 0 ms (100%), Space: 43.5 MB (91.51%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,38 +1,38 @@
 class Solution {
-    public int findMin(int[] nums) {
-        int start = 0, n = nums.length, end = n - 1;
-        while (start <= end) {
-          if(nums[start] <= nums[end]) return nums[start];
-            
-            int mid = start + (end -start)/2;
-            
-            if(nums[mid] > nums[end]){
-                start = mid +1;
-            }
-            else{
-                 end = mid;
-            }
-        }
-        return 0;
-
-    }
     // public int findMin(int[] nums) {
     //     int start = 0, n = nums.length, end = n - 1;
     //     while (start <= end) {
-    //          if (nums[start] <= nums[end]) return nums[start];
-    //         int mid = start + (end - start) / 2;
-    //         int prev = (mid + n - 1) % n;
-    //         int next = (mid + 1) % n;
-
-    //         if (nums[mid] <= nums[prev] && nums[mid] <= nums[next]) {
-    //             return nums[mid];
-    //         } else if (nums[mid] >= nums[start]) {
-    //             start = mid + 1;
-    //         } else if (nums[mid] <= nums[end]) {
-    //             end = mid - 1;
+    //       if(nums[start] <= nums[end]) return nums[start];
+            
+    //         int mid = start + (end -start)/2;
+            
+    //         if(nums[mid] > nums[end]){
+    //             start = mid +1;
+    //         }
+    //         else{
+    //              end = mid;
     //         }
     //     }
     //     return 0;
 
     // }
+    public int findMin(int[] nums) {
+        int start = 0, n = nums.length, end = n - 1;
+        while (start <= end) {
+             if (nums[start] <= nums[end]) return nums[start];
+            int mid = start + (end - start) / 2;
+            int prev = (mid + n - 1) % n;
+            int next = (mid + 1) % n;
+
+            if (nums[mid] <= nums[prev] && nums[mid] <= nums[next]) {
+                return nums[mid];
+            } else if (nums[mid] >= nums[start]) {
+                start = mid + 1;
+            } else if (nums[mid] <= nums[end]) {
+                end = mid ;
+            }
+        }
+        return 0;
+
+    }
 }
